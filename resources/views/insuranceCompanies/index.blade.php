a06efa0df0666f5b5d48b3edcbee5006bbcb5714
georgi2003 <georgidim30@gmail.com>
2020-11-13 16:23:27 +0200

    Ireate index file

--- resources/views/insuranceCompanies/index.blade.php
+++ resources/views/insuranceCompanies/index.blade.php
@@ -5,5 +5,49 @@
         </h2>
     </x-slot>
 
-    
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+  
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
+
+    <h1></h1>
+    <br>
+    <table id = "myTable" align="center" style="border:1px solid black;" class="display">
+        <thead>
+            <tr>
+                <th>№</th>
+                <th>Застрахователна компания</th>
+                <th>Изтрии</th>
+                <th>Актуализирай</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        @foreach($allInsuranceCompanies as $insuranceCompany)
+            <tr>
+                <td>{{ $loop->iteration }}</td> 
+                <td>{{ $insuranceCompany->name }}</td>
+                    <td>
+                        <form method="post" action="{{ url('insuranceCompanies') }}/{{ $insuranceCompany->id }}">
+                            @csrf
+                            @method('DELETE')
+                            <button style = "text-decoration: none; color: black;" href = "">Изтрий
+                            </button>
+                        </form>
+                    </td>
+                    <td>
+                        <button>
+                            <a style = "text-decoration: none; color: black;" href = "{{ url('insuranceCompanies') }}/{{ $insuranceCompany->id }}/edit">Актуализирай</a>
+                        </button>
+                    </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready( function () {
+            $('#myTable').DataTable();
+        } );
+    </script>
 </x-app-layout>
